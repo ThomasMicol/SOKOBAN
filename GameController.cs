96a052bfd873bbe52f0da7890c36adfe88503836
Thomas Micol <thomasmicol@gmail.com>
2017-09-28 16:25:26 +1300

    Initial Commit

---
+++ GameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class GameController
+{
+    protected IView view;
+    protected Level level;
+    protected IFileHandler fileHandler;
+
+    public GameController(IView aView)
+    {
+        view = aView;
+    }
+
+    public void StartNewGame()
+    {
+        throw new NotImplementedException("StartNewGame not implemented");
+    }
+
+    public void LoadLevel()
+    {
+        throw new NotImplementedException("Load Level has yet to be implemented");
+    }
+
+    public void LevelComplete()
+    {
+        throw new NotImplementedException("levelComplete not implemented");
+    }
+
+    protected void SaveGame()
+    {
+        throw new NotImplementedException("save has not been implemented");
+    }
+
+
+}

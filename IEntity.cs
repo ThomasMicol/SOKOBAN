cd22f9c86135f77c9e8fdc7c72c60307b6fb9adb
Thomas Micol <thomasmicol@gmail.com>
2017-11-16 19:52:58 +1300

    Added the entity and observer interfaces.

---
+++ IEntity.cs
@@ -0,0 +1,19 @@
+﻿using Assignment1___Thomas_Micol;
+
+public interface IEntity
+{
+    Location GetLocation();
+    void SetLocation(Location aLocation);
+    void SetLocationY(Directions aDir);
+    void SetLocationX(Directions aDir);
+    bool GetIsSolid();
+    EntityTypes GetEntityType();
+    void Move(Directions aDir, IEntity targetEntity, IEntity potentialPushToEntity);
+    void Move(Directions aDir, IEntity targetEntity);
+    void Move(Directions aDir);
+    void AttachNewObserver(IEntity observer);
+    void DetachObserver(IEntity observer);
+    void Notify();
+
+
+}
